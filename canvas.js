7a1202eec79b3c24edbcf987718724d4c693197c
Nevermore98 <nevermorey98@qq.com>
2021-10-09 17:41:51 +0800

    完成涂鸦画板第二版

--- canvas.js
+++ canvas.js
@@ -1,21 +1,13 @@
-let canvas = document.getElementById('drawing-board')
-let ctx = canvas.getContext('2d')
-let colorBtn = document.getElementsByClassName('color-item');
-let colorGroup = document.getElementsByClassName('color-group')
+const canvas = document.getElementById('drawing-board')
+const ctx = canvas.getContext('2d')
+const colorBtn = document.getElementsByClassName('color-item')
+const colorGroup = document.getElementsByClassName('color-group')
 
-
-const highlighter = document.getElementById('highlighter');
-const eraser = document.getElementById('eraser');
-const line = document.getElementById('line');
-const rect = document.getElementById('rect');
-const circle = document.getElementById('circle');
+const highlighter = document.getElementById('highlighter')
+const eraser = document.getElementById('eraser')
 
 canvas.width = document.documentElement.clientWidth
 canvas.height = document.documentElement.clientHeight
-
-ctx.lineWidth = 10
-ctx.lineCap = 'round'
-ctx.lineJoin = 'round'
 
 let lineColor = 'black'
 let lineWidth = 4
@@ -23,42 +15,54 @@ let lineWidth = 4
 let isDrawing = false
 let isErasing = false
 
-let lastPoint
 // 存储坐标点
-let points = [];
-let beginPoint = null;
+let points = []
+let beginPoint = null
 
 // 创建高清画布
 createHDCanvas()
 // 设置背景颜色，下载的图片背景才有颜色
-setCanvasBg('white');
-// 获取线条颜色
+setCanvasBg('white')
+// 选择线条颜色
 selectColor()
+
+highlighter.onclick = () => {
+  isErasing = false
+}
+
+eraser.onclick = () => {
+  isErasing = true
+}
+
+// 绘图
 // 判断是否为触屏设备
 let isTouchDevice = 'ontouchstart' in document.documentElement
 if (isTouchDevice) {
   // 触屏设备
   canvas.ontouchstart = (e) => {
+    //储存绘图表面
+    this.firstDot = ctx.getImageData(0, 0, canvas.width, canvas.height)
+    saveData(this.firstDot)
     isDrawing = true
     ctx.beginPath()
     beginPoint = {x: e.touches[0].clientX, y: e.touches[0].clientY}
-    points.push({x: e.touches[0].clientX, y: e.touches[0].clientY});
-    drawCircle(e.clientX, e.clientY, lineWidth / 2)
+    points.push({x: e.touches[0].clientX, y: e.touches[0].clientY})
+    drawCircle(beginPoint.x, beginPoint.y, lineWidth / 2)
   }
   canvas.ontouchmove = (e) => {
     if (!isDrawing) return
     ctx.beginPath()
-    points.push({x: e.touches[0].clientX, y: e.touches[0].clientY});
+    points.push({x: e.touches[0].clientX, y: e.touches[0].clientY})
 
     if (points.length > 3) {
-      const lastTwoPoints = points.slice(-2);
-      const controlPoint = lastTwoPoints[0];
+      const lastTwoPoints = points.slice(-2)
+      const controlPoint = lastTwoPoints[0]
       const endPoint = {
         x: (lastTwoPoints[0].x + lastTwoPoints[1].x) / 2,
         y: (lastTwoPoints[0].y + lastTwoPoints[1].y) / 2,
       }
-      drawSmoothLine(beginPoint, controlPoint, endPoint);
-      beginPoint = endPoint;
+      drawSmoothLine(beginPoint, controlPoint, endPoint)
+      beginPoint = endPoint
     }
   }
 } else {
@@ -66,68 +70,71 @@ if (isTouchDevice) {
   canvas.onmousedown = (e) => {
     //储存绘图表面
     this.firstDot = ctx.getImageData(0, 0, canvas.width, canvas.height)
-    saveData(this.firstDot);
+    saveData(this.firstDot)
     isDrawing = true
     ctx.beginPath()
     beginPoint = {x: e.clientX, y: e.clientY}
-    points.push({x: e.clientX, y: e.clientY});
-    drawCircle(e.clientX, e.clientY, lineWidth / 2)
+    points.push({x: e.clientX, y: e.clientY})
+    drawCircle(beginPoint.x, beginPoint.y, lineWidth / 2)
   }
 
   canvas.onmousemove = (e) => {
     if (!isDrawing) return
     ctx.beginPath()
-    points.push({x: e.clientX, y: e.clientY});
+    points.push({x: e.clientX, y: e.clientY})
 
     if (points.length > 3) {
-      const lastTwoPoints = points.slice(-2);
-      const controlPoint = lastTwoPoints[0];
+      const lastTwoPoints = points.slice(-2)
+      const controlPoint = lastTwoPoints[0]
       const endPoint = {
         x: (lastTwoPoints[0].x + lastTwoPoints[1].x) / 2,
         y: (lastTwoPoints[0].y + lastTwoPoints[1].y) / 2,
       }
-      drawSmoothLine(beginPoint, controlPoint, endPoint);
-      beginPoint = endPoint;
+      drawSmoothLine(beginPoint, controlPoint, endPoint)
+      beginPoint = endPoint
     }
   }
 
   canvas.onmouseup = (e) => {
-    if (!isDrawing) return;
+    if (!isDrawing) return
     ctx.beginPath()
-    points.push({x: e.clientX, y: e.clientY});
+    points.push({x: e.clientX, y: e.clientY})
 
     if (points.length > 3) {
-      const lastTwoPoints = points.slice(-2);
-      const controlPoint = lastTwoPoints[0];
-      const endPoint = lastTwoPoints[1];
+      const lastTwoPoints = points.slice(-2)
+      const controlPoint = lastTwoPoints[0]
+      const endPoint = lastTwoPoints[1]
       drawSmoothLine(beginPoint, controlPoint, endPoint)
     }
-    beginPoint = null;
-    isDrawing = false;
-    points = [];
+    beginPoint = null
+    isDrawing = false
+    points = []
   }
 
   canvas.onmouseleave = () => {
-    isDrawing = false;
+    isDrawing = false
   }
 }
 
 // 画圆形
 function drawCircle(x, y, radius) {
   if (isErasing) {
-    ctx.beginPath()
-    ctx.clip()
+    ctx.save()
     ctx.fillStyle = 'white'
     ctx.strokeStyle = 'white'
+    ctx.arc(x, y, radius, 0, Math.PI * 2)
+    ctx.fill()
+    ctx.closePath()
+    ctx.clip()
     ctx.clearRect(0, 0, canvas.width, canvas.height)
     ctx.restore()
+  } else {
+    ctx.beginPath()
+    ctx.fillStyle = lineColor
+    ctx.arc(x, y, radius, 0, Math.PI * 2)
+    ctx.fill()
+    ctx.closePath()
   }
-  ctx.save()
-  ctx.beginPath()
-  ctx.fillStyle = lineColor
-  ctx.arc(x, y, radius, 0, Math.PI * 2)
-  ctx.fill()
-  ctx.closePath()
 }
 
 // 绘制二次贝塞尔平滑曲线
@@ -136,80 +143,50 @@ function drawSmoothLine(beginPoint, controlPoint, endPoint) {
   if (isErasing) {
     ctx.save()
     ctx.globalCompositeOperation = "destination-out"
-    ctx.moveTo(beginPoint.x, beginPoint.y);
-    ctx.quadraticCurveTo(controlPoint.x, controlPoint.y, endPoint.x, endPoint.y);
-    ctx.stroke();
-    ctx.closePath();
-    ctx.clip();
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-    ctx.restore();
+    ctx.moveTo(beginPoint.x, beginPoint.y)
+    ctx.quadraticCurveTo(controlPoint.x, controlPoint.y, endPoint.x, endPoint.y)
+    ctx.stroke()
+    ctx.closePath()
+    ctx.clip()
+    ctx.clearRect(0, 0, canvas.width, canvas.height)
+    ctx.restore()
   } else {
     ctx.beginPath()
     ctx.lineCap = 'round'
     ctx.lineJoin = 'round'
-    ctx.moveTo(beginPoint.x, beginPoint.y);
-    ctx.quadraticCurveTo(controlPoint.x, controlPoint.y, endPoint.x, endPoint.y);
-    ctx.stroke();
-    ctx.closePath();
+    ctx.moveTo(beginPoint.x, beginPoint.y)
+    ctx.quadraticCurveTo(controlPoint.x, controlPoint.y, endPoint.x, endPoint.y)
+    ctx.stroke()
+    ctx.closePath()
   }
 }
 
 function setCanvasBg(color) {
-  ctx.fillStyle = color;
-  ctx.fillRect(0, 0, canvas.width, canvas.height);
-  ctx.fillStyle = 'white';
+  ctx.fillStyle = color
+  ctx.fillRect(0, 0, canvas.width, canvas.height)
+  ctx.fillStyle = 'white'
 }
 
 function selectColor() {
   ctx.beginPath()
-
   for (let i = 0; i < colorBtn.length; i++) {
     colorBtn[i].onclick = function () {
       for (let i = 0; i < colorBtn.length; i++) {
-        colorBtn[i].classList.remove('active');
-        this.classList.add('active');
-        lineColor = this.style.backgroundColor;
-        ctx.strokeStyle = lineColor;
+        colorBtn[i].classList.remove('active')
+        this.classList.add('active')
+        lineColor = this.style.backgroundColor
+        ctx.strokeStyle = lineColor
       }
     }
   }
 }
 
-eraser.onclick = () => {
-  isErasing = true;
-  console.log(colorGroup)
-  colorGroup[0].classList.add('hide')
-};
-
-highlighter.onclick = () => {
-  isErasing = false;
-  colorGroup[0].classList.remove('hide')
-};
-
-line.onclick = () => {
-  isErasing = false;
-  colorGroup[0].classList.remove('hide')
-}
-
-rect.onclick = () => {
-  isErasing = false;
-  colorGroup[0].classList.remove('hide')
-
-}
-
-circle.onclick = () => {
-  isErasing = false;
-  colorGroup[0].classList.remove('hide')
-}
-
-
-
 function createHDCanvas(w = canvas.width, h = canvas.height) {
-  let ratio = window.devicePixelRatio || 1;
-  canvas.width = w * ratio; // 实际渲染像素
-  canvas.height = h * ratio; // 实际渲染像素
-  canvas.style.width = `${w}px`; // 控制显示大小
-  canvas.style.height = `${h}px`; // 控制显示大小
+  let ratio = window.devicePixelRatio || 1
+  canvas.width = w * ratio // 实际渲染像素
+  canvas.height = h * ratio // 实际渲染像素
+  canvas.style.width = `${w}px` // 控制显示大小
+  canvas.style.height = `${h}px` // 控制显示大小
   ctx.scale(ratio, ratio)
-  return canvas;
+  return canvas
 }
--- main.js
+++ main.js
@@ -1,68 +1,55 @@
 const tools = document.getElementsByClassName('tool')
-const range = document.getElementById('range');
+const range = document.getElementById('range')
 
-
-const clear = document.getElementById('clear');
-const undo = document.getElementById('undo');
-const redo = document.getElementById('redo');
-const save = document.getElementById('save');
+const clear = document.getElementById('clear')
+const undo = document.getElementById('undo')
+const save = document.getElementById('save')
 
 // window.onbeforeunload = () => 'Reload site?'
 
-
-
-save.onclick = function () {
-    let imgUrl = canvas.toDataURL('image/png');
-    let saveA = document.createElement('a');
-    document.body.appendChild(saveA);
-    saveA.href = imgUrl;
-    saveA.download = 'pic' + (new Date).getTime();
-    saveA.target = '_blank';
-    saveA.click();
-};
-
-range.onchange = function(){
-  lineWidth = this.value;
-};
+let historyData = []
 
 selectTool()
 
-function selectTool(){
+range.onchange = function () {
+  lineWidth = this.value
+}
+
+clear.onclick = function () {
+  ctx.clearRect(0, 0, canvas.width, canvas.height)
+  setCanvasBg('white')
+}
+
+undo.onclick = () => {
+  if (historyData.length < 1) return
+  top = historyData[historyData.length - 1]
+  ctx.putImageData(historyData[historyData.length - 1], 0, 0)
+  historyData.pop()
+}
+
+save.onclick = function () {
+  let imgUrl = canvas.toDataURL('image/png')
+  let saveA = document.createElement('a')
+  document.body.appendChild(saveA)
+  saveA.href = imgUrl
+  saveA.download = 'pic' + (new Date).getTime()
+  saveA.target = '_blank'
+  saveA.click()
+}
+
+function selectTool() {
   for (let i = 0; i < tools.length; i++) {
     tools[i].onmousedown = function () {
       for (let i = 0; i < tools.length; i++) {
-        tools[i].classList.remove('active');
-        this.classList.add('active');
+        tools[i].classList.remove('active')
+        this.classList.add('active')
       }
     }
   }
 }
 
-let historyData = [];
-
-undo.onclick = () =>{
-  if(historyData.length < 1)
-    return false;
-  top = historyData[historyData.length - 1]
-  ctx.putImageData(historyData[historyData.length - 1], 0, 0)
-  historyData.pop()
-  console.log(historyData)
-};
-
-redo.onclick = function () {
-  if(historyData.length < 1)
-    return false;
-  ctx.putImageData(historyData[historyData.length - 1], 0, 0)
-  historyData.push(top)
-  console.log(historyData)
-};
-
 function saveData(data) {
-  (historyData.length === 10) && (historyData.shift());// 上限为储存10步，太多了怕挂掉
-  historyData.push(data);
+  // 撤销次数上限 10
+  (historyData.length === 10) && (historyData.shift())
+  historyData.push(data)
 }
-
-clear.onclick = function () {
-  ctx.clearRect(0, 0, canvas.width, canvas.height);
-  setCanvasBg('white');
-};
